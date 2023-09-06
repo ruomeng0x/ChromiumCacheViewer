dba223e3e76423981cfa3b29f79aa5dc30c4715e
ruomeng0x <ruomenger@foxmail.com>
2023-09-06 15:01:40 +0800

    update: 更新clang-format配置

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
+    QPushButton  button("Hello world!", nullptr);
+
     button.resize(200, 100);
     button.show();
+
     return QApplication::exec();
 }
